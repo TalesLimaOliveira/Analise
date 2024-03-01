e25ae30ac40cb7ca7815d2d5be5db04c936c7817
TalesLimaOliveira <104475181+TalesLimaOliveira@users.noreply.github.com>
2024-03-01 10:08:44 -0300

    Julia IF PCS

---
+++ IntallJulia.sh
@@ -0,0 +1,12 @@
+#!bash
+
+mkdir ~/Documentos/JuliaLang/ 
+cd ~/Documentos/JuliaLang/
+
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.1-linux-x86_64.tar.gz
+tar zxvf julia-1.8.1-linux-x86_64.tar.gz
+
+rm -rf julia-1.8.1-linux-x86_64.tar.gz
+
+# export PATH="$PATH:/home/sammy/julia-1.8.1/bin"
+# source ~/.bashrc
