b6c839c847f88edd69809d60dc7848ea99fa094b
TalesLimaOliveira <104475181+TalesLimaOliveira@users.noreply.github.com>
2024-08-05 08:33:01 -0300

    Update Bash

---
+++ install_julia.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+JULIA_VERSION="1.10.4" # any version ≥ 0.7.0
+JULIA_PACKAGES="IJulia BenchmarkTools Plots Statistics"
+
+if [ -z `which julia` ]; then
+  # Install Julia
+  JULIA_VER=`cut -d '.' -f -2 <<< "$JULIA_VERSION"`
+  echo "Installing Julia $JULIA_VERSION on the current Colab Runtime..."
+  BASE_URL="https://julialang-s3.julialang.org/bin/linux/x64"
+  URL="$BASE_URL/$JULIA_VER/julia-$JULIA_VERSION-linux-x86_64.tar.gz"
+  wget -nv $URL -O /tmp/julia.tar.gz # -nv means "not verbose"
+  tar -x -f /tmp/julia.tar.gz -C /usr/local --strip-components 1
+  rm /tmp/julia.tar.gz
+
+  # Install Packages
+  for PKG in `echo $JULIA_PACKAGES`; do
+    echo "Installing Julia package $PKG..."
+    julia -e 'using Pkg; pkg"add '$PKG'; precompile;"' &> /dev/null
+  done
+
+  echo ''
+  echo "Successfully installed `julia -v`!"
+fi
